3fea477a712c20810169549b22d813c7498256a2
Caroline Fonseca <caroline.fonseca@elgin.com.br>
2022-12-02 15:02:55 -0300

    Add data da viagem

--- android-alura-viagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ android-alura-viagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
@@ -23,12 +24,14 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
 
+        setTitle("Resumo do Pacote");
+
         //binding dos dados
         Pacote pacoteSp = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
         TextView local = findViewById(R.id.tv_local);
         local.setText(pacoteSp.getLocal());
-        
+
         ImageView img = findViewById(R.id.iv_imagem_local);
         Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacoteSp.getImagem());
         img.setImageDrawable(drawableDoPacote);
@@ -40,5 +43,18 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         TextView preco = findViewById(R.id.tv_preco);
         String moedaBr = MoedaUtil.formataParaBrasileiro(pacoteSp.getPreco());
         preco.setText(moedaBr);
+
+        TextView data = findViewById(R.id.tv_data);
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE,pacoteSp.getDias());
+        SimpleDateFormat formatoBr = new SimpleDateFormat("dd/MM");
+
+        String dataFormatadaIda = formatoBr.format(dataIda.getTime());
+        String dataFormatadaVolta = formatoBr.format(dataVolta.getTime());
+        String dataFormatadaDaViagem = dataFormatadaIda + " - "
+                +dataFormatadaVolta + " de "
+                +dataVolta.get(Calendar.YEAR);
+        data.setText(dataFormatadaDaViagem);
     }
 }
