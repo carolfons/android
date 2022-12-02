39184967b65c71c1dc15ec223de462a5246bfa3b
Caroline Fonseca <caroline.fonseca@elgin.com.br>
2022-12-02 15:41:10 -0300

    Aplicando a chain

--- android-alura-viagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ android-alura-viagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -13,48 +13,61 @@ import java.util.Calendar;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.DataUtil;
 import br.com.alura.aluraviagens.util.DiasUtil;
 import br.com.alura.aluraviagens.util.MoedaUtil;
 import br.com.alura.aluraviagens.util.ResourceUtil;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Resumo do pacote";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
 
-        setTitle("Resumo do Pacote");
+        setTitle(TITULO_APPBAR);
 
         //binding dos dados
         Pacote pacoteSp = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
-        TextView local = findViewById(R.id.tv_local);
-        local.setText(pacoteSp.getLocal());
+        mostraLocal(pacoteSp);
+        mostraImagem(pacoteSp);
+        mostraDias(pacoteSp);
+        mostraPreco(pacoteSp);
+        mostraData(pacoteSp);
+        
+    }
 
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.tv_data);
+        String dataFormatadaDaViagem = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(dataFormatadaDaViagem);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.tv_preco);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+
+
+    private void mostraDias(Pacote pacote) {
+        TextView dias = findViewById(R.id.tv_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacote.getDias());
+        dias.setText(diasEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacoteSp) {
         ImageView img = findViewById(R.id.iv_imagem_local);
         Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacoteSp.getImagem());
         img.setImageDrawable(drawableDoPacote);
+    }
 
-        TextView dias = findViewById(R.id.tv_dias);
-        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSp.getDias());
-        dias.setText(diasEmTexto);
-
-        TextView preco = findViewById(R.id.tv_preco);
-        String moedaBr = MoedaUtil.formataParaBrasileiro(pacoteSp.getPreco());
-        preco.setText(moedaBr);
-
-        TextView data = findViewById(R.id.tv_data);
-        Calendar dataIda = Calendar.getInstance();
-        Calendar dataVolta = Calendar.getInstance();
-        dataVolta.add(Calendar.DATE,pacoteSp.getDias());
-        SimpleDateFormat formatoBr = new SimpleDateFormat("dd/MM");
-
-        String dataFormatadaIda = formatoBr.format(dataIda.getTime());
-        String dataFormatadaVolta = formatoBr.format(dataVolta.getTime());
-        String dataFormatadaDaViagem = dataFormatadaIda + " - "
-                +dataFormatadaVolta + " de "
-                +dataVolta.get(Calendar.YEAR);
-        data.setText(dataFormatadaDaViagem);
+    private void mostraLocal(Pacote pacoteSp) {
+        TextView local = findViewById(R.id.tv_local);
+        local.setText(pacoteSp.getLocal());
     }
 }
