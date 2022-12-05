04765f8a8b576379da752a779da3be21b21c70bb
Caroline Fonseca <caroline.fonseca@elgin.com.br>
2022-12-05 12:41:12 -0300

    Refatoração

--- android-alura-viagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
+++ android-alura-viagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
@@ -3,14 +3,34 @@ package br.com.alura.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSp = new Pacote("São Paulo", "sao_paulo_sp",2,new BigDecimal("243.99"));
+        mostraPreco(pacoteSp);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.tv_pagamento_preco);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
